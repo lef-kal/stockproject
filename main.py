07c597c1ca493f9267f6d0379a42770ca2c0ddcf
lefkal <kalonarisl@gmail.com>
2021-03-10 20:48:35 +0200

    actual prediction

--- main.py
+++ main.py
@@ -48,5 +48,52 @@ model.add(Dense(units=1))#next value
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
 
+##load data
 
+test_start =dt.datetime(2020,1,1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset =pd.concat((data['Close'], test_data['Close']), axis=0)
+
+### input
+
+model_inputs =total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1, 1)
+model_inputs = scaler.transform(model_inputs)
+
+#predict test
+
+x_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days:x, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predicted_prices = model.predict(x_test)
+predicted_prices = scaler.inverse_transform(predicted_prices)
+
+#plots
+
+plt.plot(actual_prices, color="black", label=f"Actual {company} Price")
+plt.plot(predicted_prices, color="green", label=f"Predicted {company} Price")
+plt.title(f"{company} Share Price")
+plt.xlabel("Time")
+plt.ylabel(f"{company} Share Price")
+plt.legend()
+plt.show()
+
+##actual prediction
+
+real_data =[model_inputs[len(model_inputs) +1 - prediction_days:len(model_inputs+1)]]
+real_data = np.array(real_data)
+real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+print(f"Prediction: {prediction}")
 
